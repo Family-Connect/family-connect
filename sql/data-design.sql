4717b8ebdedcf63e8fb80277af450db32fb20c11
Anthony Garcia <garcia.anthony577@gmail.com>
2018-11-02 10:00:23 -0600

    Merge branch 'master' of C:\Users\Carlos\Desktop\bootcamp\family-connect with conflicts.

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -1,14 +1,17 @@
-DROP TABLE IF EXISTS family;
+ALTER DATABASE foo CHARACTER SET uft8 COLLATE utf8_unicode_ci;
 
--- create family table
+DROP TABLE IF EXISTS user;
 
-CREATE TABLE family (
-	-- primary key
-	familyId BINARY(16) NOT NULL,
-	-- attributes
-	familyName VARCHAR(64) NOT NULL,
-	-- prevent duplicate data
-	UNIQUE(familyName),
-	-- primary key
-	PRIMARY KEY(familyId)
-);
+CREATE TABLE user (
+	userId BINARY(16) NOT NULL,
+	userFamilyId BINARY(16) NOT NULL,
+	userActivationToken VARCHAR (128) NOT NULL,
+	userAvatar VARCHAR (128) NOT NULL,
+	userDisplayName VARCHAR (128) NOT NULL,
+	userEmail VARCHAR (128) NOT NULL,
+	userHash CHAR (96) NOT NULL,
+	userPhoneNumber CHAR (7) NOT NULL,
+	userPrivilege VARCHAR (250) NOT NULL,
+	userTimeZone CHAR (4) NOT NULL,
+	userName VARCHAR (96) NOT NULL
+)
