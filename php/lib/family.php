eef67e15d817a917820883341b1f34f06f5e86f2
Michael Bovee <michael.j.bovee@gmail.com>
2018-11-05 16:45:41 -0700

    added family.php file to lib directory https://app.asana.com/0/897247891702380/897247891702383

---
+++ php/lib/family.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michael
+ * Date: 11/5/18
+ * Time: 4:43 PM
+ */
+
+namespace FamConn\FamilyConnect;
+require_once ("../Classes/autoload.php");
+
+$date = new \DateTime('now');
+
+$instance = new Family("b02fe7f8-69df-4915-9448-166358834b83", "family name");
+
+var_dump($instance);
