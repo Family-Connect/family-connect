6938c44a45c0e88b45183b62ace890e4fe1f5505
felizmunoz <fmunoz273@gmail.com>
2018-11-05 11:00:38 -0700

    began comment class under php. lines1-20. https://app.asana.com/0/897247891702380/897247891702386

---
+++ php/Classes/comment.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: felizmunoz
+ * Date: 11/5/18
+ * Time: 10:47 AM
+ */
+
+namspace fmunoz\family-connect;
+require_once("sutoload.php");
+require_once(dirname(path:__Dir__, levels: 2) . "/vendor/autoload.php");
+use Ramsey\Uuid\Uuid;
+
+/**
+ * This is the comment section of Family Connect
+ * comments can come from all users in a group
+ *
+ * @author Feliz Munoz <fmunoz11@cnm.edu>
+ * @version 3.0.0
+ */
