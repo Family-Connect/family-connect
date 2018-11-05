fa45f54149e259a401f7f8f64b6a234beaef8ec3
Anthony Garcia <garcia.anthony577@gmail.com>
2018-11-05 11:45:06 -0700

    added attributes to class https://app.asana.com/0/897247891702380/897247891702382

--- php/Classes/user.php
+++ php/Classes/user.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace ;
+namespace agarcia\family-connect ;
 
 require_once("autoload.php");
 require_once(dirname(__DIR__,2)."/vendor/autoload.php");
@@ -11,9 +11,51 @@ use Ramsey\Uuid\Uuid;
  *version 1.0.0
  */
 
-class User implements \JsonSerializable {
+class User {
 	use ValidateUuid;
 	/**
-	 * id for this
+	 * id for this User; this is the primary key
+	 * @var Uuid $userId
 	 */
+	private $userId;
+	/**
+	 * id of this Family in which the user belongs to; this is a foreign key
+	 * @var Uuid $userFamilyId
+	 */
+	private $userFamilyId;
+	/**
+	 * Activation Token for the user
+	 * @var string $userActivationToken
+	 */
+	private $userActivationToken;
+	/**
+	 * Icon that represents the user
+	 * @var string $userAvatar
+	 */
+	private $userAvatar;
+	/**
+	 *Text that identifies the user
+	 * @var string $userDisplayName
+	 */
+	private $userDisplayName;
+	/**
+	 * Email the user uses to log in
+	 * @var string $userEmail
+	 */
+	private $userEmail;
+	/**
+	 * Hash for the user
+	 * @var string $userHash
+	 */
+	private $userHash;
+	/**
+	 * Phone number user uses as information
+	 * @var string $userPhoneNumber
+	 */
+	private $userPhoneNumber;
+	/**
+	 * Privilege allows user to do administrative tasks
+	 @var string $userPrivilege
+	 */
+	private $userPrivilege;
 }
