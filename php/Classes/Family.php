fbc515aad0f3c2c90c3fe37e8b27a551c3b40bff
Michael Bovee <michael.j.bovee@gmail.com>
2018-11-05 10:49:44 -0700

    added Family.php file with very very basic skeleton https://app.asana.com/0/897247891702380/897247891702383

---
+++ php/Classes/Family.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michael
+ * Date: 11/5/18
+ * Time: 10:42 AM
+ */
+
+namespace ;
+
+require_once("autoload.php");
+require_once(dirname(__DIR__, 2) . "/vendor/autoload.php");
+
+use Ramsey\Uuid\Uuid;
+/** This is the top-level entity for the Family Connect web app.
+ *
+ * @author Michael Bovee <michael.j.bovee@gmail.com>
+ * version 1.0.0
+ */
+
+class Family {
+
+}
