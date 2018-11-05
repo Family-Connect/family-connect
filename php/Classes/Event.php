2d82a67374c2f78ac1c335cf053582642f05bc76
sharonromero <sh-aron@hotmail.com>
2018-11-05 12:04:57 -0700

    Added mutator method for event id https://app.asana.com/0/897247891702380/897247891702385

--- php/Classes/Event.php
+++ php/Classes/Event.php
@@ -13,57 +13,87 @@ use Ramsey\Uuid\Uuid;
  * @version 1.0.0
  **/
 class Event {
-use ValidateUuid;
-/**id for the Event, this is the primary key.
-* @var uuid eventId
-**/
-private $eventId;
-/**
-* id of the event that the family creates; this is a foreign key.
-* @var uuid $eventFamilyId;
-**/
-private $eventFamilyId;
-/**
-* id of the event that the user creates; this is a foreign key.
-* @var $eventUserId;
-**/
-private $eventUserId;
-/**
-*content of the event.
-* @var string $eventContent;
-**/
-private $eventContent;
+	use ValidateUuid;
+	/**id for the Event, this is the primary key.
+	 * @var uuid eventId
+	 **/
+	private $eventId;
+	/**
+	 * id of the event that the family creates; this is a foreign key.
+	 * @var uuid $eventFamilyId ;
+	 **/
+	private $eventFamilyId;
+	/**
+	 * id of the event that the user creates; this is a foreign key.
+	 * @var $eventUserId ;
+	 **/
+	private $eventUserId;
+	/**
+	 *content of the event.
+	 * @var string $eventContent ;
+	 **/
+	private $eventContent;
 	/**
 	 * End date for the event.
 	 * @var $eventEndDate
-	**/
-private $eventEndDate;
+	 **/
+	private $eventEndDate;
 	/**
 	 * Name of the event
 	 * @var $eventName
-	**/
-private $eventName;
+	 **/
+	private $eventName;
 	/**
 	 * This is the start date of the event.
 	 * @var $eventStartDate
-	**/
-private $eventStartDate;
+	 **/
+	private $eventStartDate;
 
 
-
-public function __construct($newEventId, $newEventFamilyId, $newEventUserId, string $newEventContent, $newEventEndDate,
-									 $newEventName,$newEventStartDate) {
-	try {
-		$this->setEventId($newEventId);
-		$this->setEventFamilyId($newEventFamilyId);
-		$this->setEventUserId($newEventUserId);
-		$this->setEventContent($newEventContent);
-		$this->setEventEndDate($newEventEndDate);
-		$this->setEventName($newEventName);
-		$this->setEventStartDate($newEventStartDate);
-	} //determine what exception was thrown
-	catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
-		$exceptionType = get_class($exception);
-		throw(new $exceptionType($exception->getMessage(), 0, $exception));
+	public function __construct($newEventId, $newEventFamilyId, $newEventUserId, string $newEventContent, $newEventEndDate,
+										 $newEventName, $newEventStartDate) {
+		try {
+			$this->setEventId($newEventId);
+			$this->setEventFamilyId($newEventFamilyId);
+			$this->setEventUserId($newEventUserId);
+			$this->setEventContent($newEventContent);
+			$this->setEventEndDate($newEventEndDate);
+			$this->setEventName($newEventName);
+			$this->setEventStartDate($newEventStartDate);
+		} //determine what exception was thrown
+		catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
 	}
+
+	/**
+	 * accessor method for event id
+	 *
+	 * @return Uuid value for eventId
+	 **/
+	public function getEventId(): Uuid {
+		return ($this->eventId);
+
+		//this outside of class
+		//$event->get event id();
+	}
+
+**
+* mutator method for event id
+* @param uuid $newEventId new value of event id
+* @throws \RangeException if $newEventId is not positive
+* @throws \TypeError if $newEventId is not a uuid
+**/
+	public function setEventId($newEventId): void {
+		try {
+			$uuid = self::validateUuid($newEventId);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+		}
+
+		//convert and store the article id
+		$this->eventId = $uuid;
+	}
+
+
 }
