48d4dd729f7d4b1ff13cd5bd80a623b03a1c4e7e
Michael Bovee <michael.j.bovee@gmail.com>
2018-11-05 10:58:38 -0700

    added variables and constructor documentation https://app.asana.com/0/897247891702380/897247891702383

--- php/Classes/Family.php
+++ php/Classes/Family.php
@@ -19,5 +19,26 @@ use Ramsey\Uuid\Uuid;
  */
 
 class Family {
+	use ValidateUuid;
+	/**
+	 * id for this family, this is the primary key
+	 * @var Uuid $familyId
+	 */
+	private $familyId;
+	/**
+	 * display name for family entity, this is a unique attribute
+	 * @var string $familyName
+	 */
+	private $familyName;
 
+	/**
+	 * constructor for this family
+	 * @param string|Uuid $newFamilyId if this family is null or if a new family
+	 * @param string $newFamilyName - name of family
+	 * @throws \InvalidArgumentException if data types aren't valid
+	 * @throws \RangeException if data values are not correct length
+	 * @throws \TypeError if data values are not the correct type
+	 * @throws \Exception for any other mysqli_sql_exception
+	 * @Documentation https://php.net/manual/en/language.oop5.decon.php
+	 */
 }
