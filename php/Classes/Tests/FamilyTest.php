db0a48188a1279298939d2c4ca233badd687e9fa
Michael Bovee <michael.j.bovee@gmail.com>
2018-11-11 14:04:07 -0700

    added insert, update, delete tests to FamilyTest https://app.asana.com/0/inbox/897279518871982/897247891702383/897270382966881

--- php/Classes/Tests/FamilyTest.php
+++ php/Classes/Tests/FamilyTest.php
@@ -42,9 +42,31 @@ class FamilyTest extends FamilyConnectTest {
 		parent::setUp();
 	}
 
-	public function testInsertValidFamily() : void  {
+	/**
+	 * test inserting a valid Family and verify the actual mySQL data matches
+	 */
+	public function testInsertValidFamily() : void {
 		// count rows, save for later
-		$numRows = $this->getConnection()->getRowCount("tweet");
+		$numRows = $this->getConnection()->getRowCount("family");
+
+		// create a new Family and insert to mySQL
+		$familyId =  "eac7aad3-28f7-4293-847b-87fd7a2ad225";
+		$family = new Family($familyId, $this->VALID_FAMILYNAME);
+		$family->insert($this->getPDO());
+
+		// grab the data from mySQL and make sure fields match expectations
+		$pdoFamily = Family::getFamilyByFamilyId($this->getPDO(), $family->getFamilyId());
+		$this->assertEquals($pdoFamily->getFamilyId(), $familyId);
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("family"));
+		$this->assertEquals($pdoFamily->getFamilyName(), $this->VALID_FAMILYNAME);
+	}
+
+	/**
+	 * test inserting a Family, editing it, then updating it
+	 */
+	public function testUpdateValidFamily() : void  {
+		// count rows, save for later
+		$numRows = $this->getConnection()->getRowCount("family");
 
 		//create a new family and insert to mySQL
 		$familyId =  "3772ebb6-abcb-48d5-a549-ab0640f2fd67";
@@ -54,5 +76,33 @@ class FamilyTest extends FamilyConnectTest {
 		// edit family and update it in mySQL
 		$family->setFamilyName($this->VALID_FAMILYNAME2);
 		$family->update($this->getPDO());
+
+		// grab the data from mySQL and make sure fields match expectations
+		$pdoFamily = Family::getFamilyByFamilyId($this->getPDO(), $family->getFamilyId());
+		$this->assertEquals($pdoFamily->getFamilyId(), $familyId);
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("family"));
+		$this->assertEquals($pdoFamily->getFamilyName(), $this->VALID_FAMILYNAME2);
+	}
+
+	/**
+	 * test creating a Family and then deleting it
+	 */
+	public function testDeleteValidFamily() : void {
+		// count rows, save for later
+		$numRows = $this->getConnection()->getRowCount("family");
+
+		//create a new family and insert to mySQL
+		$familyId =  "bc289be5-8f77-4972-8d7b-9192a9d07232";
+		$family = new Family($familyId, $this->VALID_FAMILYNAME);
+		$family->insert($this->getPDO());
+
+		// delete the family from mySQL
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("family"));
+		$family->delete($this->getPDO());
+
+		//grab the data from mySQL and make sure that the Family does not exist
+		$pdoFamily = Family::getFamilyByFamilyId($this->getPDO(), $family->getFamilyId());
+		$this->assertNull($pdoFamily);
+		$this->assertEquals($numRows, $this->getConnection()->getRowCount("family"));
 	}
 }
