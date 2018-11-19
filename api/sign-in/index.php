c93bc01aba43296f502ef3dd2385b9b975cae4ec
sharonromero <sh-aron@hotmail.com>
2018-11-19 16:31:38 -0700

    Added start session, grab mysql statement, determine which HTTP method is being used, and if method is post handle the sign in logic.

--- api/sign-in/index.php
+++ api/sign-in/index.php
@@ -26,4 +26,28 @@ try {
 	//determine which HTTP method is being used
 	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
 
+	//If method is post handle the sign in logic
+	if($method === "POST") {
+
+			//make sure the XSRF Token is valid
+			verifyXsrf();
+
+			//process the request content and decode the json object into a php object
+			$requestContent = file_get_contents("php://input");
+			$requestObject = json_decode($requestContent);
+
+			//check to make sure the password and email field is not empty.s
+			if(empty($requestObject->userEmail) === true) {
+				throw(new \InvalidArgumentException("Wrong email address.", 401));
+			} else {
+						$userEmail = filter_var($requestObject->userEmail, FILTER_SANITIZE_EMAIL);
+			}
+
+			if(empty($requestObject->userPassword) === true) {
+				throw(new \InvalidArgumentException("Must enter a password.", 401));
+			} else {
+						$userPassword = $requestObject->userPassword;
+			}
+	}
+
 }
