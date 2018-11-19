47689b08c7a70b92e48a7f84e44b97d6515a1d70
sharonromero <sh-aron@hotmail.com>
2018-11-19 15:58:34 -0700

    Started api for handling sign-in

--- api/sign-in/index.php
+++ api/sign-in/index.php
@@ -1,3 +1,29 @@
 <?php
 require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
-require_once dirname()
+require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
+require_once dirname(__DIR__, 3) . "/php/lib/jwt.php";
+require_once ("/etc/apache2/capstone-mysql/encrypted-config.php");
+use FamConn\FamilyConnect\User;
+
+/**
+ * api for handling sign-in
+ *
+ * @author sromero130
+ **/
+//prepare and empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+try {
+
+	//start session
+	if(session_status() !== PHP_SESSION_ACTIVE) {
+		session_start();
+	}
+	//grab mySQL statement
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/cohort22/familyconnect.ini");
+
+	//determine which HTTP method is being used
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
+}
