d89c141db3ed58d635035753c9e7c89e6808bb4d
Michael Bovee <michael.j.bovee@gmail.com>
2018-11-19 20:03:07 -0700

    put down code for sign out api https://app.asana.com/0/897247891702380/916511001837218

---
+++ api/sign-out/index.php
@@ -0,0 +1,42 @@
+<?php
+
+require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
+require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
+require_once ("/etc/apache2/capstone-mysql/Secrets.php");
+
+// verify the xsrf challenge
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+
+// prepare default error message
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+try {
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/cohort22/familyconnect");
+
+	//determine which HTTP method was used
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+	if($method === "GET") {
+		$_SESSION = [];
+		$reply->message = "You are now signed out.";
+	}
+	else {
+		throw(new \InvalidArgumentException("Invalid HTTP method request."));
+	}
+} catch(Exception $exception){
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+} catch(TypeError $typeError) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+}
+
+header("Content-type: application/json");
+if($reply->data === null) {
+	unset($reply->data);
+}
+
+// encode and return reply to front end
+echo json_encode($reply);
