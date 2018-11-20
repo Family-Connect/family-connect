cd614e1d96bd69be7a52f9905c1c297cbd4e48f9
Michael Bovee <michael.j.bovee@gmail.com>
2018-11-20 10:02:15 -0700

    put down some more code for family api https://app.asana.com/0/897247891702380/916511001837215

--- api/family/index.php
+++ api/family/index.php
@@ -15,3 +15,47 @@ use FamConn\FamilyConnect\{Family};
  *
  * @author Michael Bovee
  */
+
+// verify session / start session if not active
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+
+// prepare empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
+try {
+	// grab mySQL connection
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/cohort22/familyconnect");
+
+	// determine which HTTP method was used
+	$method = $_SERVER["HTTP_X_HTTP_METHOD"] ?? $_SERVER["REQUEST_METHOD"];
+
+	// sanitize input
+	$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$familyName = filter_input(INPUT_GET, "familyName", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+
+	// verify id is valid for methods requiring it
+	if(($method === "DELETE" || $method === "PUT") && (empty($id) === true)) {
+		throw(new InvalidArgumentException("id cannot be empty or negative", 405));
+	}
+
+	if($method === "GET") {
+		// set XSRF cookie
+		setXsrfCookie();
+
+		//gets family by content
+		if(empty($id) === false) {
+			$reply->data = Family::getFamilyByFamilyId($pdo, $id);
+		} else if(empty($familyName) === false) {
+			$reply->data = Family::getFamilyByFamilyName($pdo, $familyName);
+		}
+	} elseif($method === "PUT") {
+
+	}
+
+
+}
+
