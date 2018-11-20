ebd3253b62ad8c497fae11ca5aea4dcf26db4745
Michael Bovee <michael.j.bovee@gmail.com>
2018-11-20 09:47:36 -0700

    put down code for family api https://app.asana.com/0/897247891702380/916511001837215

---
+++ api/family/index.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once(dirname(__DIR__, 3). "/vendor/autoload.php");
+require_once(dirname(__DIR__, 3). "/php/classes/autoload.php");
+require_once(dirname(__DIR__, 3). "/php/lib/jwt.php");
+require_once(dirname(__DIR__, 3). "/php/lib/xsrf.php");
+require_once(dirname(__DIR__, 3). "/php/lib/uuid.php");
+
+require_once ("/etc/apache2/capstone-mysql/Secrets.php");
+
+use FamConn\FamilyConnect\{Family};
+
+/**
+ * API for Family
+ *
+ * @author Michael Bovee
+ */
