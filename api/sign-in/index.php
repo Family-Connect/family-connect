616e0909a26db43b789113695fcd77841040ec39
sharonromero <sh-aron@hotmail.com>
2018-11-20 10:01:19 -0700

    Fixed spacing/indentations on index.php in sign-in. https://app.asana.com/0/897247891702380/916511001837217

--- api/sign-in/index.php
+++ api/sign-in/index.php
@@ -16,77 +16,77 @@ $reply->status = 200;
 $reply->data = null;
 try {
 
-	//start session
-	if(session_status() !== PHP_SESSION_ACTIVE) {
-		session_start();
-	}
-	//grab mySQL statement
-	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/cohort22/familyconnect.ini");
+		//start session
+		if(session_status() !== PHP_SESSION_ACTIVE) {
+				session_start();
+		}
+		//grab mySQL statement
+		$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/cohort22/familyconnect.ini");
 
-	//determine which HTTP method is being used
-	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+		//determine which HTTP method is being used
+		$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : 		  			$_SERVER["REQUEST_METHOD"];
 
-	//If method is post handle the sign in logic
-	if($method === "POST") {
+		//If method is post handle the sign in logic
+		if($method === "POST") {
 
-		//make sure the XSRF Token is valid
-		verifyXsrf();
+				//make sure the XSRF Token is valid
+				verifyXsrf();
 
-		//process the request content and decode the json object into a php object
-		$requestContent = file_get_contents("php://input");
-		$requestObject = json_decode($requestContent);
+				//process the request content and decode the json object into a php object
+				$requestContent = file_get_contents("php://input");
+				$requestObject = json_decode($requestContent);
 
-		//check to make sure the password and email field is not empty.s
-		if(empty($requestObject->userEmail) === true) {
-			throw(new \InvalidArgumentException("Wrong email address.", 401));
+				//check to make sure the password and email field is not empty.s
+				if(empty($requestObject->userEmail) === true) {
+					throw(new \InvalidArgumentException("Wrong email address.", 401));
+				} else {
+							$userEmail = filter_var($requestObject->userEmail, FILTER_SANITIZE_EMAIL);
+				}
+
+				if(empty($requestObject->userPassword) === true) {
+							throw(new \InvalidArgumentException("Must enter a password.", 401));
+				} else {
+							$userPassword = $requestObject->userPassword;
+				}
+
+				//grab the user from the database by the email provided
+				$user = User::getUserByUserEmail($pdo, $userEmail);
+				if(empty($user) === true) {
+							throw(new \InvalidArgumentException("Invalid Email", 401));
+				}
+				$user->setUserActivationToken(null);
+				$user->update($pdo);
+
+				//if the user activation token is not null throw an error
+				if($user->getUserActivationToken() !== null) {
+						throw (new \InvalidArgumentException ("you are not allowed to sign in unless you have 						activated your account", 403));
+				}
+
+				//verify hash is correct
+				if(password_verify($requestObject->userPassword, $user->getUserHash()) === false) {
+						throw(new \InvalidArgumentException("Password or email is incorrect.", 401));
+				}
+
+				//grab user from database and put into a session
+				$user = User::getUserByUserId($pdo, $user->getUserId());
+
+				$_SESSION["user"] = $user;
+
+				//create the Auth payload
+				$authObject = (object)[
+						"userId" => $user->getUserId(),
+						"userAtHandle" => $user->getUserAtHandle()
+				];
+
+				//create and set the JWT TOKEN
+				setJWTAndAuthHeader("auth", $authObject);
+
+				$reply->message = "Sign in was successful.";
 		} else {
-			$userEmail = filter_var($requestObject->userEmail, FILTER_SANITIZE_EMAIL);
+				throw(new \InvalidArgumentException("Invalid HTTP method request", 418));
 		}
 
-		if(empty($requestObject->userPassword) === true) {
-			throw(new \InvalidArgumentException("Must enter a password.", 401));
-		} else {
-			$userPassword = $requestObject->userPassword;
-		}
-
-		//grab the user from the database by the email provided
-		$user = User::getUserByUserEmail($pdo, $userEmail);
-		if(empty($user) === true) {
-			throw(new \InvalidArgumentException("Invalid Email", 401));
-		}
-		$user->setUserActivationToken(null);
-		$user->update($pdo);
-
-		//if the user activation token is not null throw an error
-		if($user->getUserActivationToken() !== null) {
-			throw (new \InvalidArgumentException ("you are not allowed to sign in unless you have activated 					your account", 403));
-		}
-
-		//verify hash is correct
-		if(password_verify($requestObject->userPassword, $user->getUserHash()) === false) {
-			throw(new \InvalidArgumentException("Password or email is incorrect.", 401));
-		}
-
-		//grab user from database and put into a session
-		$user = User::getUserByUserId($pdo, $user->getUserId());
-
-		$_SESSION["user"] = $user;
-
-		//create the Auth payload
-		$authObject = (object)[
-			"userId" => $user->getUserId(),
-			"userAtHandle" => $user->getUserAtHandle()
-		];
-
-		//create and set the JWT TOKEN
-		setJWTAndAuthHeader("auth", $authObject);
-
-		$reply->message = "Sign in was successful.";
-	} else {
-		throw(new \InvalidArgumentException("Invalid HTTP method request", 418));
-	}
-
-	//if an exception is thrown update the
+		//if an exception is thrown update the
 } catch(Exception | TypeError $exception) {
 	$reply->status = $exception->getCode();
 	$reply->message = $exception->getMessage();
