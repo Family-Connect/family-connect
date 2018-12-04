ea361fd88b2e96854df1db80c3f5fa5702f1e6dc
Michael Bovee <michael.j.bovee@gmail.com>
2018-12-04 14:16:49 -0700

    working on sidebar

--- static-ui/template.php
+++ static-ui/template.php
@@ -1,2 +1,29 @@
 <?php require_once ("head-utils.php");?>
 
+<div class="wrapper">
+
+	<!-- Sidebar -->
+	<nav id="sidebar" class="sidebar col-3">
+		<div class="sidebar-header">
+			<h3>Family Name</h3>
+		</div>
+
+		<ul class="users">
+			<li>
+				<a href="#">User1</a>
+			</li>
+		</ul>
+	</nav>
+
+	<!-- Page Content -->
+	<div id="content" class="col-9">
+		<div class="container-fluid">
+
+			<button type="button" id="sidebarCollapse" class="btn btn-info">
+				<i class="fas fa-align-left"></i>
+			</button>
+
+		</div>
+	</div>
+
+</div>
