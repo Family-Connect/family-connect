2ed7afb9538b2ece0d684f811714aa33891dd00d
felizmunoz <fmunoz273@gmail.com>
2018-12-05 09:13:47 -0700

    added settings button to detailed-task.php and some page content

--- static-ui/detailed-task.php
+++ static-ui/detailed-task.php
@@ -28,14 +28,30 @@
 				<button type="button" id="plusButton" class="btn plusButton">
 					<i class="fas fa-cog"></i>
 				</button>
+
+				<button type="button" id="settingsButton" class="btn settingsButton">
+					<i class="fas fa-cog"></i>
+				</button>
 			</div>
 		</div>
 
 		<!-- Actual content (not sidebar) - edit below -->
 
-		<div class="content container">
-			<h4>Task Name</h4>
-			<h4>Task Creator </h4>
-		</div>
-	</div>
+		<div class="content container-fluid">
+			<div class="task-head"
+				<h4>Task Name</h4>
+				<br>
+				<h6>Task Creator</h6>
+			</div>
+
+			<div class="task-body row">
+				<div class="task-description-comments col-6">
+					<div class="task-description">
+						<h4>Description</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div>
+
+			<div class="task-comment">
+				<h4>Task Comments</h4>
+			</div>
 </div>
