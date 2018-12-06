9cd82a068eb4d8b1ece06f9ff4cc4ebfbcac2860
felizmunoz <fmunoz273@gmail.com>
2018-12-06 14:49:08 -0700

    updated comment.service.ts

--- src/app/shared/services/comment.service.ts
+++ src/app/shared/services/comment.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpParams} from "@angular/common/http";
 
 import {Comment} from "../interfaces/comment";
 import {Status} from "../interfaces/status";
@@ -11,18 +11,55 @@ export class PostService {
 	constructor(protected http: HttpClient) {
 	}
 
-	private postUrl = "apis/post/";
+	//define the API endpoint
+	private commentUrl = "apis/post/";
 
-	getAllComments() : Observable<Comment[]> {
-		return(this.http.get<Comment[]>(this.postUrl));
+	// call to the comment API and delete the comment in question
+	deleteComment(commentId: string): Observable<Status> {
+		return (this.http.delete<Status>(this.commentUrl + commentId));
 	}
 
-	getCommentByCommentId(postId : number) : Observable<Comment> {
-		return(this.http.get<Comment>(this.postUrl +postId));
+	// call to the comment API and edit the comment in question
+	editComment(comment: Comment): Observable<Status> {
+		return (this.http.put<Status>(this.commentUrl + comment.commentId, comment));
 	}
 
-	createComment(post : Comment) : Observable<Status> {
-		return(this.http.post<Status>(this.postUrl, +post)
-		);
+	// call to the comment API and creat the comment in question
+	createComment(comment: Comment): Observable<Status> {
+		return (this.http.post<Status>(this.commentUrl, +comment));
 	}
+
+	// call to the comment API and get a comment object based on its Id
+	getComment(commentId: string): Observable<Comment> {
+		return (this.http.get<Comment>(this.commentUrl + commentId));
+	}
+
+	// call to the comment API and get an array of comments based off the eventId
+	getCommentbyEventId(commentEventId: string): Observable<any[]> {
+		return (this.http.get<any[]>(this.commentUrl, {params: new HttpParams().set("commentEventId", commentEventId)}));
+	}
+
+	// call to the comment API and get an array of comments based off the taskId
+	getCommentbyTaskId(commentTaskId: string): Observable<any[]> {
+		return (this.http.get<any[]>(this.commentUrl, {params: new HttpParams().set("commentTaskId", commentTaskId)}));
+	}
+
+	// call to the comment API and get an array of commentss based off the userId
+	getCommentbyUserId(commentUserId: string): Observable<any[]> {
+		return (this.http.get<any[]>(this.commentUrl, {params: new HttpParams().set("commentUserId", commentUserId)}));
+	}
+
+	// call to the comment API and get an array of comments based off the profileId
+	getCommentbyProfileId(commentProfileId: string): Observable<Comment[]> {
+		return (this.http.get<Comment[]>(this.commentUrl, {params: new HttpParams().set("commentProfileId", commentProfileId)}));
+	}
+
+	// call to the comment API and get an array of comments based off the commentContent
+	getCommentByContent(commentContent: string): Observable<Comment[]> {
+		return (this.http.get<Comment[]>(this.commentUrl, {params: new HttpParams().set("commentContent", commentContent)}));
+	}
+
 }
+
+
+
