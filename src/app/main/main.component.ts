2e060104741435c554aa639b61de916893cd9dce
sharonromero <sh-aron@hotmail.com>
2018-12-08 15:22:26 -0700

    Change accordion to example George gave to us.

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,11 +1,21 @@
-import {Component, OnInit} from "@angular/core";
+<!--import {Component, OnInit} from "@angular/core";-->
+
+import {Component} from "@angular/core";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {Status} from "../shared/interfaces/status";
 
 @Component({
-	template: require("./main.component.html")
-})
-
-export class MainComponent{
-
+	selector: "ngbd-accordion-basic",
+	templateUrl: "./main.component.html"
+	})
+	export class NgbdAccordionBasic {
 }
+
+
+// @Component({
+// 	template: require("./main.component.html")
+// })
+//
+// export class MainComponent{
+//
+// }
