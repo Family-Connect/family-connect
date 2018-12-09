939cc3ba85cb572712bc6eb162de3163fb397757
felizmunoz <fmunoz273@gmail.com>
2018-12-09 13:03:51 -0700

    wired with angular the form in detailed-task-component.html!!! finally!

--- src/app/detailed-task/detailed-task.component.ts
+++ src/app/detailed-task/detailed-task.component.ts
@@ -7,20 +7,57 @@ import {Status} from "../shared/interfaces/status";
 import {User} from "../shared/interfaces/user";
 import {Task} from "../shared/interfaces/task";
 import {ActivatedRoute} from "@angular/router";
+import {Comment} from "../shared/interfaces/comment";
 
 @Component({
 	template: require("./detailed-task.component.html")
 })
 
 export class DetailedTaskComponent implements OnInit {
-	status: Status = {status:null, message:null, type:null};
+	user: User = {userId: null, userFamilyId:null, userAvatar:null, userDisplayName:null, userEmail:null, userPhoneNumber:null};
+	comment: Comment = {commentId: null, commentEventId: null, commentTaskId: null, commentUserId: null, commentContent: null, commentDate:null};
+	comments: Comment[];
 	task: Task = {taskId:null, taskEventId:null, taskUserId:null, taskDescription:null, taskDueDate:null, taskIsComplete:null, taskName:null};
 	taskId: string = this.route.snapshot.params["taskId"];
+	commentOnTaskForm: FormGroup;
+	status: Status = {status:null, message:null, type:null};
 
 	constructor(private commentService: CommentService, private taskService: TaskService, private userService: UserService, private formBuilder: FormBuilder, private route: ActivatedRoute) { }
 
 	ngOnInit(): void {
 		this.taskService.getTask(this.taskId).subscribe(task => this.task = task);
+		this.loadComments();
+
+		this.commentOnTaskForm = this.formBuilder.group({
+			taskCommentContent : ["", [Validators.maxLength(855), Validators.required]]
+		})
+	}
+
+	loadComments() : any {
+		this.commentService.getCommentByTaskId(this.taskId).subscribe(comments => this.comments = comments);
+	}
+
+	commentOnTask() : any {
+		let comment: Comment = {
+			commentId: null,
+			commentEventId: this.taskId,
+			commentTaskId: null,
+			commentUserId: this.user.userId,
+			commentContent: this.commentOnTaskForm.value.commentContent,
+			commentDate: null
+		};
+
+		this.commentService.createComment(comment)
+			.subscribe(status => {
+				this.status = status;
+
+				if(status.status === 200) {
+					this.commentOnTaskForm.reset();
+					this.loadComments();
+				} else {
+					alert(status.message);
+				}
+			})
 	}
 
 }
