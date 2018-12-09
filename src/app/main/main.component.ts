84107069a47684ebe6c9c68933bd94432209376b
sharonromero <sh-aron@hotmail.com>
2018-12-09 14:18:58 -0700

    Fixed h4s on main.component.html. Started writing main.component.ts.

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,12 +1,15 @@
 import {Component, OnInit} from "@angular/core";
-
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+
 import {Status} from "../shared/interfaces/status";
+import {User} from "../shared/interfaces/user";
 
 @Component({
 	template: require("./main.component.html")
 })
 
-export class MainComponent{
+export class MainComponent implements OnInit {
+	user: User = {userId:null, userFamilyId:null, userAvatar:null, userDisplayName:null, userEmail:null, userPhoneNumber:null};
+	event:
 
 }
