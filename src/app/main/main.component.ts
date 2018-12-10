2ec428f643c17865f495945ccdedf95ac2660533
sharonromero <sh-aron@hotmail.com>
2018-12-09 17:01:01 -0700

    Made changes with George.

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -5,9 +5,10 @@ import {UserService} from "../shared/services/user.service";
 import {Status} from "../shared/interfaces/status";
 import {User} from "../shared/interfaces/user";
 import {Task} from "../shared/interfaces/task";
+import {Event} from "../shared/interfaces/event";
 import {ActivatedRoute} from "@angular/router";
-import {TaskData} from "@angular/core/src/testability/testability";
 import {EventService} from "../shared/services/event.service";
+import {JwtHelperService} from "@auth0/angular-jwt";
 
 @Component({
 	template: require("./main.component.html")
@@ -15,25 +16,33 @@ import {EventService} from "../shared/services/event.service";
 
 export class MainComponent implements OnInit {
 	user: User = {userId:null, userFamilyId:null, userAvatar:null, userDisplayName:null, userEmail:null, userPhoneNumber:null};
-	event: Event = {eventId:null, eventUserId:null, eventContent:null, eventEndDate:null, eventName:null, eventStartDate:null};
+	event: Event = {eventId:null, eventFamilyId:null, eventUserId:null, eventContent:null, eventEndDate:null, eventName:null, eventStartDate:null};
 	eventId: string = this.route.snapshot.params["eventId"];
-	task: Task = {taskId:null, taskEventId:null, taskUserId:null, taskDescription:null, taskDueDate:null, taskName:null};
+	task: Task = {taskId:null, taskEventId:null, taskUserId:null, taskDescription:null, taskDueDate:null, taskIsComplete:null, taskName:null};
 	taskId: string = this.route.snapshot.params["taskId"];
+	events: Event[];
 
-	constructor(private userService: UserService, private eventService: EventService, private taskService: TaskService){ }
+	jwt = this.jwtHelperService.decodeToken(window.localStorage.getItem("jwt-token"));
+
+	constructor(private userService: UserService, private eventService: EventService, private taskService: TaskService, private route : ActivatedRoute,private jwtHelperService: JwtHelperService){ }
 
 	ngOnInit(): void {
-		this.taskService.getTask(this.taskId).subscribe(task => this.task = task);
+		this.taskService.getTaskByFamilyId(this.jwt.auth.taskId).subscribe(task => this.task = task);
 		this.loadTasks();
 
-		this.eventService.getEvent(this.eventId).subscribe(event => this.event = event);
+
+		this.eventService.getEventByFamilyId(this.jwt.auth.familyId).subscribe(event => this.events = event);
 	}
 
 		loadTasks() : any {
-			this.taskService.getTaskByTaskId(this.taskId).subscribe(tasks => this.tasks = tasks);
+			this.taskService.getTask(this.taskId).subscribe(tasks => this.task = tasks);
 		}
 
 		loadEvents() : any {
-			this.eventService.getEventByEventId(this.eventId).subscribe(events => this.events = events);
+			this.eventService.getEventByFamilyId(this.eventId).subscribe(events => this.events = events);
+		}
+
+		justMe() : any {
+			this.eventService.getEventByUserId(this.jwt.auth.userId).subscribe(events => this.events = events);
 		}
 }
