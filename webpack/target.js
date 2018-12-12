17a186d29a612c70152e030b6c327a4d50779bfb
felizmunoz <fmunoz273@gmail.com>
2018-12-12 11:27:05 -0700

    updated the link for profile user

--- webpack/target.js
+++ webpack/target.js
@@ -1,4 +1,4 @@
 module.exports = function() {
 	//path to your deployment API directory
-	return basepath = "https://bootcamp-coders.cnm.edu/~agarcia707/family-connect/public_html"; //Your URL here
+	return basepath = "https://bootcamp-coders.cnm.edu/~fmunoz11/family-connect/public_html"; //Your URL here
 };
